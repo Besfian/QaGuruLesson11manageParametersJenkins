a8a4fbbfe2d4dea9d14e5561d1c38861419b27b4
besfian <besfian@mail.ru>
2021-12-14 02:52:42 +0300

    новый билдгрэдл

---
+++ src/test/java/tests/OwnerTests.java
@@ -0,0 +1,26 @@
+package tests;
+
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.String.format;
+
+@Tag("owner")
+public class OwnerTests {
+    public CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
+    @Test
+    void readCredentialsTest() {
+        String login = credentials.login();
+        String password = credentials.password();
+
+        System.out.println(login);
+        System.out.println(password);
+
+        String message = format("https://%s:%s@selenoid.autotests.cloud/wd/hub/", login, password);
+        System.out.println(message);
+    }}
+
